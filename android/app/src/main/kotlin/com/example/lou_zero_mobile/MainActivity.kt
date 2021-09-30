8c1104b255301358d8359b079865bd5b3620f466
mmarianosilva <mmarianosilva0@gmail.com>
2021-09-30 10:17:20 -0600

    add inital boilerplate from tutorial

--- android/app/src/main/kotlin/com/example/lou_zero_mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/lou_zero_mobile/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.lou_zero_mobile
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    @Override
+    void onCreate(){
+        AppCenter.start(getApplication(), "2bfa1527-5bfa-41d8-9105-fa7e23d2bd75",
+                  Analytics.class, Crashes.class);
+    }
 }
