e80491ee7c4d841bb6a0b38cef9af5eb66e3dfd4
mmarianosilva <mmarianosilva0@gmail.com>
2021-11-08 15:23:42 -0800

    set props, gradient, custom font, layout, etc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         body: DashBoard(),
       ),
@@ -25,7 +26,37 @@ class DashBoard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        child: Text('Hello LouZERO.'),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.purple.shade900,
+            Colors.pink.shade900,
+          ],
+        )),
+        child: Center(
+          child: Text(
+            'LouZERO',
+            style: TextStyle(
+                shadows: const <Shadow>[
+                  Shadow(
+                    offset: Offset(0, 0),
+                    blurRadius: 55.0,
+                    color: Color.fromARGB(80, 0, 0, 0),
+                  ),
+                  Shadow(
+                    offset: Offset(0, 3),
+                    blurRadius: 5.0,
+                    color: Colors.black,
+                  ),
+                ],
+                fontFamily: 'Noto',
+                fontSize: 100,
+                fontWeight: FontWeight.normal,
+                color: Colors.teal.shade50),
+          ),
+        ),
       ),
     );
   }
