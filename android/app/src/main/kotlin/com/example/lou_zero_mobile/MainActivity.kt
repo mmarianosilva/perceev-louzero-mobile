1a6b2de8b9061d6efd8aca0ec23cdcf53e81b528
mmarianosilva <mmarianosilva0@gmail.com>
2021-11-09 22:29:31 +0000

    Automatic deployment by github-actions

--- android/app/src/main/kotlin/com/example/lou_zero_mobile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.louzero
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('Who needs tests?', () {
-    int expectedNumber = 1;
-    expect(expectedNumber, 1);
-  });
-}
