57bd5aea20f9f2d5e3d18cb532d41cbc6b84b4cc
mmarianosilva <mmarianosilva0@gmail.com>
2021-11-10 15:26:33 -0800

    form widgets

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,54 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: DashBoard(),
-      ),
+    return MaterialApp(
+      title: 'AppBro',
+      theme: ThemeData(primarySwatch: Colors.grey),
+      home: LogIn(title: 'Form Start'),
     );
+  }
+}
+
+class LogIn extends StatefulWidget {
+  LogIn({Key? key, this.title = 'title'}) : super(key: key);
+  final String title;
+
+  @override
+  _LogInState createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  final formKey = GlobalKey<FormState>();
+  String? _email, _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(widget.title)),
+        body: Card(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Email:'),
+                    validator: (String? value) {
+                      return (value != null && value.contains('@'))
+                          ? 'Do not use the @ char.'
+                          : null;
+                    },
+                    onSaved: (String? value) {
+                      _email = value;
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
 
